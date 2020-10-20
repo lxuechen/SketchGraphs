30cbc675ad8e274a3617b3f11884a7bfc9d0f172
lxuechen <12689993+lxuechen@users.noreply.github.com>
2020-10-19 22:18:07 -0400

    Add shortcut scripts.

---
+++ scripts/autoconstraint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python -m sketchgraphs_models.autoconstraint.train --dataset_train sg_t16_train.npy
+
---
+++ scripts/autoregressive.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python -m sketchgraphs_models.graph.train --dataset_train sg_t16_train.npy
+
