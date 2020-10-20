c4105d54fabd4aa832bca5792f51a3dd229ff46c
lxuechen <12689993+lxuechen@users.noreply.github.com>
2020-10-19 22:33:34 -0400

    Refactor shortcut.

--- scripts/autoconstraint.sh
+++ scripts/autoconstraint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-python -m sketchgraphs_models.autoconstraint.train --dataset_train sg_t16_train.npy
+python -m sketchgraphs_models.autoconstraint.train \
+    --dataset_train sequence_data/sg_t16_train.npy \
+    --batch_size 128 \
+    --num_workers 8
 
